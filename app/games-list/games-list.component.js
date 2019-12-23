83cfcee815ac93d487d89f6bedef95f8038cc916
Vitaly <vitaly.z.vz@gmail.com>
2019-12-24 00:24:01 +0300

    'add_merch_category'

--- app/games-list/games-list.component.js
+++ app/games-list/games-list.component.js
@@ -11,15 +11,17 @@ angular.module('gamesList')
       this.sortBy = "Name.en"
       this.category = 0
       this.locStor = lsService
-      this.showMainList = false
-      this.favAlert = true
-      this.favList = true
+      this.hideMainList = false
+      this.hideFavLimitAlert = true
+      this.hideEmptyMess = true
+      this.hideMerch = true
       console.log($scope) //Scope {$id: 2, ....,  _: ƒ ()
       $http.get('http://127.0.0.1:8887/data.json')
         .then(response => {
           self.data = response.data
           this.games = this.data.games
           this.categories = this.data.categories
+          this.merchants = _.values(this.data.merchants)
         })
       this.changeGamesPerPage = function (e) {
         this.gamesOnPage = e.target.innerHTML
@@ -35,25 +37,31 @@ angular.module('gamesList')
         (!this.reverse) ? this.reverse = '-' : this.reverse = ''
       }
       this.changeCategory = function (id) {
+        this.hideMerch = true
         this.page = 1
         this.category = +id
-        if (this.category === -1) {
-          this.locStor.length > 0 ? this.favList = true : this.favList = false
-          this.showMainList = true
+        if (this.category === -1) { // === Favorite Games
+          this.locStor.length > 0 ? this.hideEmptyMess = true : this.hideEmptyMess = false
+          this.hideMainList = true
         }
         else {
-          this.favList = true
-          this.showMainList = false
+          this.hideEmptyMess = true
+          this.hideMainList = false
         }
       }
       this.addFavorite = function (id, game) {
 
         if (_.includes(this.locStor, game)) { _.remove(this.locStor, function (v) { return _.isEqual(v, game) }) }
-        else if (this.locStor.length < 5) { this.locStor.push(game) } else { this.favAlert = false }
+        else if (this.locStor.length < 5) { this.locStor.push(game) } else { this.hideFavLimitAlert = false }
         localStorage.setItem('favorite', JSON.stringify(this.locStor))
       }
       this.hideFavAlert = function () {
-        this.favAlert = true
+        this.hideFavLimitAlert = true
+      }
+      this.viewMerchList = function () {
+        this.category = -2
+        this.hideMerch = false
+        this.hideEmptyMess = true
       }
     }]
   })
